30948620ec0a4ecb80b0a89f97bca7613dbdb6bb
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-02-12 12:29:33 +0000

    Got socket.io server running, among other things.
    Added factory, controllers, split the site into views and got the ng-app
    module to work.

---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+var RuChat = angular.module('RuChat', ['ngRoute']);
+
+RuChat.config(
+    function($routeProvider) {
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'partials/login.html',
+                controller: 'loginController'
+            })
+            .when('/rooms/:user', {
+                templateUrl: 'partials/rooms.html',
+                controller: 'roomsController'
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+    }
+);
+
+RuChat.controller('loginController', function($scope, $location, $rootScope, $routeParams, socket) {
+
+    $scope.errorMessage = '';
+    $scope.nickname = '';
+
+    $scope.login = function() {
+        if ($scope.nickname === '') {
+            $scope.errorMessage = 'Please choose a username before continuing!';
+        } else {
+            socket.emit('adduser', $scope.nickname, function(available) {
+                if (available) {
+                    $location.path('/rooms/' + $scope.nickname);
+                } else {
+                    $scope.errorMessage = 'This nick-name is already taken!';
+                }
+            });
+        }
+    };
+});
+
+RuChat.controller('roomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
+	// TODO: Query chat server for active rooms
+	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+	$scope.currentUser = $routeParams.user;
+});
